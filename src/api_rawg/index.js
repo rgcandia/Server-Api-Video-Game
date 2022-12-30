7fccdee7119913b8048d8d4855f40da1b302ea9d
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2022-12-30 02:25:00 -0300

    se arregla import otra vez

--- src/api_rawg/index.js
+++ src/api_rawg/index.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch-commonjs')
 
 //trae la info genres de la API externa, y crea las tablas.
 const setTableGenres = (model,url,key)=>{
